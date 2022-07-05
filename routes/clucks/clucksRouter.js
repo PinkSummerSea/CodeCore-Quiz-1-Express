94efd9cba3cb0aae8626d2a460c9e08c23560bec
Xiaojun Lin <xiaojunlin@XiaojundeMacBook-Pro.local>
2022-07-05 13:36:33 -0700

    challenge part 1 completed. human friendly relative date displayed on index page.

--- routes/clucks/clucksRouter.js
+++ routes/clucks/clucksRouter.js
@@ -2,15 +2,51 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../../db/client');
 
+function timeDifference(current, previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+         return 'Just Now';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+}
+
 router.get('/new', (req, res) => {
     res.render('clucks/new')
 })
 
 router.get('/', (req, res) => {
+    
     knex('clucks')
     .orderBy('created_at', 'desc')
     .then(clucks => {
-        res.render('clucks/index', { clucks })
+        res.render('clucks/index', { clucks, timeDifference })
     })
 })
 
